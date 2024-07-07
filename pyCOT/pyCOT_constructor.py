ac1e2ae1f46c03fbdf32628f882c79d5e8e5b88a
Diego A. Pacheco <diegopachecos@gmail.com>
2024-07-07 18:26:51 -0400

    Relocate some bitarray methods

--- pyCOT/pyCOT_constructor.py
+++ pyCOT/pyCOT_constructor.py
@@ -5,6 +5,8 @@ from bitarray import bitarray as bt
 import networkx as nx
 # import pandas as pd
 
+import py_data_conversion as dc
+
 # Created by Tomas Veloz - github.com/tveloz
 ############################################################################################################
 # Summary: Create/Load/Visualize reaction networks and compute COT-related properties#######################
@@ -62,22 +64,7 @@ class pyCOT:
         self.RnMsupp = RnMsupp
         self.RnMprod = RnMprod
 
-    ########################################################################################################
-    #############Vector of positions to bt transformations##################################################
-    ########################################################################################################
 
-    def get_id_from_bt(self, bt: bt) -> list[int]:
-        """Function that returns a vector from bitarray representation."""
-        vec = [i for i in range(len(bt)) if bt[i] == 1]
-        return vec
-
-    def get_bt_from_id(self, vec: Sequence[int], size) -> bt:
-        """Function that returns bitarray from vector representation."""
-        bt_array = bt(size)
-        bt_array.setall(0)
-        for i in vec:
-            bt_array[i] = 1
-        return bt_array
 
   ##########################################################################################################
   #############bt From/To str representations###############################################################
@@ -145,19 +132,10 @@ class pyCOT:
                     selected_reactions.append(self.RnStr[i])
             return selected_reactions
 
-    def get_bt_abstraction_from_vector(self, vec, t):
-        """Function that returns bitarray of species with value larger than t in a vector."""
-        bitarray = bt()
-        for i in range(len(vec)):
-            if vec[i] > t:
-                bitarray.append(True)
-            else:
-                bitarray.append(False)
-        return(bitarray)
 
     def get_species_abstraction_from_vector(self, vec, t):
         """Function that returns a the list of species string with value larger than t in a vector"""
-        bitarray = self.get_bt_abstraction_from_vector(vec, t)
+        bitarray = dc.get_bt_abstraction_from_vector(vec, t)
         return(self.get_species_from_bt(bitarray))
 
     def get_supp_bt_from_reaction(self, reaction_name, t=0):
@@ -168,7 +146,7 @@ class pyCOT:
         else:
             reaction_index = self.RnStr.index(reaction_name)
             support_vec = self.RnMsupp[reaction_index]
-            support_bitarray = self.get_bt_abstraction_from_vector(support_vec, t)
+            support_bitarray = dc.get_bt_abstraction_from_vector(support_vec, t)
         return support_bitarray
 
     def get_prod_bt_from_reaction(self,reaction_name,t=0):
@@ -179,7 +157,7 @@ class pyCOT:
         else:
             reaction_index = self.RnStr.index(reaction_name)
             product_vec = self.RnMprod[reaction_index]
-            product_bitarray = self.get_bt_abstraction_from_vector(
+            product_bitarray = dc.get_bt_abstraction_from_vector(
                 product_vec, t)
         return product_bitarray
 
--- pyCOT/py_data_conversion.py
+++ pyCOT/py_data_conversion.py
@@ -13,7 +13,7 @@ from itertools import combinations
 import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup as bs
-from bitarray import bitarray as bt
+from bitarray import bitarray
 from bitarray import frozenbitarray as fbt
 # from scipy.optimize import linprog
 import matplotlib.pyplot as plt
@@ -24,5 +24,29 @@ import networkx as nx
 from Display import *
 
 
-# Function that returns bitarray from vector representation    
+########################################################################################################
+#############Vector of positions to bt transformations##################################################
+########################################################################################################
 
+def get_id_from_bt(bt: bitarray) -> list[int]:
+    """Function that returns a vector from bitarray representation."""
+    vec = [i for i in range(len(bt)) if bt[i] == 1]
+    return vec
+
+def get_bt_from_id(vec: Sequence[int], size) -> bitarray:
+    """Function that returns bitarray from vector representation."""
+    bt_array = bitarray(size)
+    bt_array.setall(0)
+    for i in vec:
+        bt_array[i] = 1
+    return bt_array
+
+def get_bt_abstraction_from_vector(vec: Sequence[int], t: int):
+    """Function that returns bitarray of species with value larger than t in a vector."""
+    bt = bitarray()
+    for i in range(len(vec)):
+        if vec[i] > t:
+            bt.append(True)
+        else:
+            bt.append(False)
+    return(bt)
