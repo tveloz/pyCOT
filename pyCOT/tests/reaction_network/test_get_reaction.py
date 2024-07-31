d7d936b96ec1e0127f629fdf6f240737f1b22d03
Diego A. Pacheco <diegopachecos@gmail.com>
2024-07-30 22:33:27 -0400

    Fix tests for ReactionNetwork.get_reaction

---
+++ pyCOT/tests/reaction_network/test_get_reaction.py
@@ -0,0 +1,30 @@
+import pytest
+from pyCOT.rn_rustworkx import ReactionNetwork, Reaction, InvalidNode
+
+@pytest.fixture
+def rn():
+    rn = ReactionNetwork()
+    rn.add_species("A", 10)
+    rn.add_species("B", 20)
+    rn.add_reaction("R1", ["A"], ["B"], [1], [1], 1)
+    return rn
+
+def test_get_reaction_existing(rn: ReactionNetwork):
+    reaction = rn.get_reaction("R1")
+
+    assert isinstance(reaction, Reaction)
+    assert reaction.name() == "R1"
+
+    reactant_edge = reaction.support_edges()[0]
+    assert reactant_edge.type == "reactant"
+    assert reactant_edge.source == 0
+    assert reactant_edge.target == 2
+    
+    product_edge = reaction.products_edges()[0]
+    assert product_edge.type == "product"
+    assert product_edge.source == 2
+    assert product_edge.target == 1
+
+def test_get_reaction_not_existing(rn: ReactionNetwork):
+    with pytest.raises(InvalidNode):
+        rn.get_reaction("R2")
