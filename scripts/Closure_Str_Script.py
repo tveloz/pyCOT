ac9807e9cdfacb51411cdf341e5e8495afe6ebb3
Tomas Veloz <tveloz@gmail.com>
2024-12-06 14:48:18 +0100

    advancing with org computation

--- scripts/Closure_Str_Script.py
+++ scripts/Closure_Str_Script.py
@@ -47,15 +47,16 @@ file_path = '../../networks/testing/RedPDoSR00.txt'
 file_path = '../../networks/biomodels_interesting/Biomodel_724.txt'
 #file_path = '../../networks/testing/ERCs_test.txt'
 #file_path = '../../networks/testing/Synergy_test.txt'
-#file_path = '../../networks/testing/Farm.txt'
+file_path = 'networks/testing/Farm.txt'
 #file_path = '../../networks/testing/MSORN_test1.txt'
 #file_path = '../../networks/testing/MSORN_test2.txt'
-#file_path = '../../networks/RandomAlife/RN_Ns_40_Norg_24_id_316.txt'
-#file_path = '../../networks/biomodels_interesting/central_ecoli.txt'
-file_path = '../../networks/biomodels_all/BIOMD0000000086_url.xml'
+#file_path = 'networks/RandomAlife/RN_Ns_40_Norg_20_id_396.txt'
+# file_path = 'networks/biomodels_interesting/central_ecoli.txt'
+# file_path = 'networks/biomodels_all_txt/BIOMD0000001044.txt'
+# file_path = 'networks/biomodels_all_txt/BIOMD0000001003.txt'
+# file_path = 'networks/GPT_Generated/EColi.txt   '
 #file_path = '../../networks/biomodels_interesting/BIOMD0000000237_manyOrgs.xml'
-#testRN2 = load_pyCOT_from_file(file_path)
-testRN2 = load_pyCOT_from_Sbml(file_path)
+testRN2 = load_pyCOT_from_file(file_path)
 
 # reaction_graph = build_graph(testRN2)
 # visualize_graph(reaction_graph)
@@ -68,10 +69,10 @@ print("reacsBt")
 print(testRN2.RnBt)
 print("Reac")
 print(testRN2.RnStr)
-print("SuppVec")
-print(testRN2.RnVecS)
-print("ProdVec")
-print(testRN2.RnVecP)
+print("MSupp")
+print(testRN2.RnMsupp)
+print("MProd")
+print(testRN2.RnMprod)
 #reaction_graph = build_graph(testRN2)
 #visualize_graph(testRN2)
 
@@ -93,198 +94,136 @@ print("check direct containment")
 for g in dc:
       print(g)
 print("the list of "+str(len(dc))+" direct containments")
-
-# print("check synergies")
-syn=get_synergies(testRN2,erc)
-for g in syn:
-      print(g)
-print("the list of "+str(len(syn))+" synergies")
-
-d_syn=get_direct_synergies(testRN2,erc,dc,syn)
-
-for g in d_syn:
-      print(g)
-print("the list of "+str(len(d_syn))+" direct synergies")
-
-
-sorn=second_order_network(testRN2,erc,dc,d_syn)
-print("The list of second order network species")
-for i in range(len(sorn[0][0])):
-      print(str(sorn[0][0][i])+" has lenght "+str(len(sorn[0][1][i])))
-            
-print("the list of "+str(len(sorn[1]))+" second order reaction network")
-for i in range(len(sorn[1])):    
-    print("r"+str(i)+": "+str(sorn[1][i]))
-
-msorn=MSORN(sorn)
-print(str(msorn.get_connected_species_to_species("E4")))
-start_time = time.time()
-cl_pth=Closed_paths_backward(msorn)
-end_time = time.time()
-execution_time = end_time - start_time
-print("Execution time computing closed paths", execution_time, "seconds")
-print("number of closed paths ="+str(len(cl_pth)))
-# for g in cl_pth:
-#     print(g)    
-gen=Closed_paths_to_Sp(cl_pth,sorn)
-print("************ANALYSIS RESULTS******************")
-
-print("We have "+str(len(gen))+" closed sets")
-gen_original=gen.copy()
-gen=remove_duplicates(gen)
-print("After eliminating duplicates we have "+str(len(gen))+" closed sets")
-for g in gen:
-      print(testRN2.get_reactions_from_species(g))
-SSM=0
-sizeSSM=0
-sizenSSM=0
-for g in gen:
-    if testRN2.is_semi_self_maintaining(g):
-        print("Closed set of size of size "+str(len(g))+ " is SSM")
-        SSM=SSM+1
-        sizeSSM=sizeSSM+len(g)
-    else:
-        print("Closed set of size "+str(len(g))+ " is not SSM")
-        sizenSSM=sizenSSM+len(g)
-nSSM=len(gen)-SSM
 print("Number of species ="+str(len(testRN2.SpStr)))
 print("Number of reactions ="+str(len(testRN2.RnStr)))
-print("Number of ERCs ="+str(len(msorn.SpStr)))
-print("SSM = "+str(SSM)+", not SSM ="+str(nSSM))
-print("relative size SSM ="+str(sizeSSM)+"/"+str(SSM))
-print("relative size nSSM ="+str(sizenSSM)+"/"+str(nSSM))
+print("Number of ERCs ="+str(len(erc)))
 
 
-#Example visualization of a reaction network
-# reaction_network = [
-#     ['A', 'B'],
-#     ['B', 'C'],
-#     ['A', 'D', 'E'],
-#     ['B', 'D', 'F']
-# ]
+#Plotting basics of ERC structure
+erc_sizes = [len(entry[1]) for entry in erc]
+reaction_counts = [len(entry[2]) for entry in erc]
 
-# reaction_network=gen[2]
-# #print(reaction_network)
-# # Build and visualize the graph
-# reaction_graph = build_graph(reaction_network)
-# visualize_graph(reaction_graph)
+# Step 2: Sort the data
+sorted_erc_sizes = sorted(erc_sizes)
+sorted_reaction_counts = sorted(reaction_counts)
 
-# Example usage
+# Step 3: Plot the sorted reaction counts and ERC sizes
+plt.figure(figsize=(15, 5))
+
+# # Plot 1: Reaction counts sorted by size
+# plt.subplot(1, 3, 1)
+# plt.plot(sorted_reaction_counts, marker='o')
+# plt.title("Reaction Counts (Sorted)")
+# plt.xlabel("Index")
+# plt.ylabel("Number of Reactions")
+
+# # Plot 2: ERC sizes sorted by size
+# plt.subplot(1, 3, 2)
+# plt.plot(sorted_erc_sizes, marker='o')
+# plt.title("ERC Sizes (Sorted)")
+# plt.xlabel("Index")
+# plt.ylabel("Size of ERC (Number of Species)")
+
+# Plot 3: ERC size vs. Reaction count scatter plot
+plt.subplot(1, 3, 1)
+plt.scatter(erc_sizes, reaction_counts, alpha=0.7)
+plt.title("ERC Size vs Number of Reactions")
+plt.xlabel("Size of ERC (Number of Species)")
+plt.ylabel("Number of Reactions")
+
+# Plot 4: Histograms of both ERC sizes and reaction counts
+plt.subplot(1, 3, 2)
+plt.hist(erc_sizes, alpha=0.5, label='ERC Sizes')
+plt.hist(reaction_counts, alpha=0.5, label='Reaction Counts')
+plt.legend(loc='upper right')
+plt.title("Histograms of ERC Sizes and Reaction Counts")
+plt.xlabel("Size")
+plt.ylabel("Frequency")
+
+plt.show()
+
+
+
+# Example list of containment relationships (your ERCs)
+containment_list = dc
+
+# Step 1: Initialize a directed graph (forest)
 G = nx.DiGraph()
-G.add_nodes_from([
-    (1, {'type': 'species'}),
-    (2, {'type': 'species'}),
-    (3, {'type': 'species'}),
-    (4, {'type': 'reaction'}),
-    (5, {'type': 'reaction'}),
-])
-G.add_edges_from([(1, 4), (4, 2), (2, 5), (5, 3)])
 
-species_node = 2  # Node representing the species you're interested in
+# Step 2: Add edges based on containment list
+for containment in containment_list:
+    G.add_edge(containment[0], containment[1])
 
-#CBL=Closed_by_levels(testRN2)
-#print(str(CBL))
+# Step 3: Visualize the forest
+# Using a layout that is good for hierarchical structures
+pos = nx.spring_layout(G, seed=42)  # You can also try nx.kamada_kawai_layout or nx.spectral_layout
 
-# Example usage:
-
-# Accessing attributes of the instance
-# print("Species Bitarray:", testRN2.SpBt)
-# print("Species Vector:", RN.SpVec)
-# print("Species Names:", RN.SpStr)
-# print("Reaction Bitarray (Require Support):", RN.RnBtS)
-# print("Reaction Bitarray (Produce Support):", RN.RnBtP)
-# print("Reaction Vector (Require Support):", RN.RnVecS)
-# print("Reaction Vector (Produce Support):", RN.RnVecP)
-# print("Reaction Names:", RN.RnStr)
-# mgen=minimal_generators(testRN2)
-# print("the list of minimal generators")
-
-# for g in mgen:
-#     print(g)
-# print(len(gen))
-# print(len(mgen))
-#print(len(mgen))
-#print("specs")
-#print(testRN2.SpStr)
-# print("specsBt")
-# print(testRN2.SpBt)
-# print("reacsBt")
-# print(testRN2.RnBt)
-# print("Reac")
-# print(testRN2.RnStr)
-# print("SuppVec")
-# print(testRN2.RnVecS)
-# print("ProdVec")
-# print(testRN2.RnVecP[1])
-# print("bt_from_species")
-# print(testRN2.get_bt_from_species([]))
-# print("species_from_bt")
-# print(testRN2.get_species_from_bt(bt('0001')))
-
-# print("bt_from_reactions")
-# print(testRN2.get_bt_from_reactions(['R1']))
-
-# print("reactions_from_bt")
-# print(testRN2.get_reactions_from_bt(bt('00000000')))
-# print("support_bt_from_reaction")
-#print(testRN2.get_supp_bt_from_reaction('R1'))
-# print("products_bt_from_reaction")
-# print(testRN2.get_prod_bt_from_reaction('R8'))
-# print(str(testRN2.SpStr[1:3]))
-# print("reactions_from_species")
-# print(testRN2.get_reactions_from_species([]))
-
-# print("get prod from reactions")
-# print(testRN2.get_prod_from_reactions([]))
-# print("get supp from reactions")
-# print(testRN2.get_supp_from_reactions([]))
-# file_path = '../../networks/autopoietic_ext2.txt'
-# testRN2 = load_pyCOT_from_file(file_path)
-# print("bt_from_reactions ")
-# print(testRN2.get_bt_from_reactions([]))
-# print("bt_from_species")
-# print(testRN2.get_bt_from_species([]))
+# Draw the graph with labels
+plt.figure(figsize=(8, 6))
+nx.draw(G, pos, with_labels=True, node_size=2000, node_color="skyblue", font_size=10, font_weight="bold", arrows=True)
+plt.title("ERC Containment Hierarchy (Forest)")
+plt.show()
 
 
-# print(testRN2.RnStr)
+# # print("check synergies")
+# syn=get_synergies(testRN2,erc)
+# for g in syn:
+#       print(g)
+# print("the list of "+str(len(syn))+" synergies")
 
-# print("get inflow")
-# print(testRN2.get_inflow())
+# d_syn=get_direct_synergies(testRN2,erc,dc,syn)
 
-# print("get outflow")
-# print(testRN2.get_outflow())
+# for g in d_syn:
+#       print(g)
+# print("the list of "+str(len(d_syn))+" direct synergies")
 
 
-# print("get_supp_from_reactions")
-# print(testRN2.get_supp_from_reactions(testRN2.RnStr[4]))
+# sorn=second_order_network(testRN2,erc,dc,d_syn)
+# print("The list of second order network species")
+# for i in range(len(sorn[0][0])):
+#       print(str(sorn[0][0][i])+" has lenght "+str(len(sorn[0][1][i])))
+            
+# print("the list of "+str(len(sorn[1]))+" second order reaction network")
+# for i in range(len(sorn[1])):    
+#     print("r"+str(i)+": "+str(sorn[1][i]))
 
-# print("get_prod_from_reactions "+str(testRN2.RnStr[0:2]))
-# print(testRN2.get_prod_from_reactions(['R1']))
-# print("get_reactions_from_species")
-# print(testRN2.get_reactions_from_species([]))
+# msorn=MSORN(sorn)
+# print(str(msorn.get_connected_species_to_species("E4")))
+# start_time = time.time()
+# cl_pth=Closed_paths_backward(msorn)
+# end_time = time.time()
+# execution_time = end_time - start_time
+# print("Execution time computing closed paths", execution_time, "seconds")
+# print("number of closed paths ="+str(len(cl_pth)))
+# # for g in cl_pth:
+# #     print(g)    
+# gen=Closed_paths_to_Sp(cl_pth,sorn)
+# print("************ANALYSIS RESULTS******************")
 
-# print("get get_prod_from_species")
-# print(testRN2.get_prod_from_species(['l','s2']))
-# print("get_reactions_consuming_species")
-# print(testRN2.get_reactions_consuming_species(['l','s1']))      
-# print("get_reactions_producing_species")
-# print(testRN2.get_reactions_producing_species(['l','s1']))        
+# print("We have "+str(len(gen))+" closed sets")
+# gen_original=gen.copy()
+# gen=remove_duplicates(gen)
+# print("After eliminating duplicates we have "+str(len(gen))+" closed sets")
+# for g in gen:
+#       print(testRN2.get_reactions_from_species(g))
+# SSM=0
+# sizeSSM=0
+# sizenSSM=0
+# for g in gen:
+#     if testRN2.is_semi_self_maintaining(g):
+#         print("Closed set of size of size "+str(len(g))+ " is SSM")
+#         SSM=SSM+1
+#         sizeSSM=sizeSSM+len(g)
+#     else:
+#         print("Closed set of size "+str(len(g))+ " is not SSM")
+#         sizenSSM=sizenSSM+len(g)
+# nSSM=len(gen)-SSM
+# print("Number of species ="+str(len(testRN2.SpStr)))
+# print("Number of reactions ="+str(len(testRN2.RnStr)))
+# print("Number of ERCs ="+str(len(msorn.SpStr)))
+# print("SSM = "+str(SSM)+", not SSM ="+str(nSSM))
+# print("relative size SSM ="+str(sizeSSM)+"/"+str(SSM))
+# print("relative size nSSM ="+str(sizenSSM)+"/"+str(nSSM))
 
-#print("get connected species to species")
-#print(testRN2.get_connected_species_to_species(['l']))
 
-# # print("get directly connected species to species")
-# # print(testRN2.get_immediately_connected_species_to_species(['s2','x']))
-# print("get forward connected species to species")
-# print(testRN2.get_forward_connected_species_to_species('x'))
-# print("get reactions consuming")
-# print(testRN2.get_reactions_consuming_species('s1'))
-
-# print("get reactions producing")
-# print(testRN2.get_reactions_producing_species('s1'))
-
-# print("is_closed")
-# print(testRN2.is_closed(['s1','s2']))
-# print("is_semi_self_maintaining")
-# print(testRN2.is_semi_self_maintaining(['d','s1']))
 
